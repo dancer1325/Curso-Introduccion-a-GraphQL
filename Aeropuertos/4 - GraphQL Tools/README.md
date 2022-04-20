0dd8adc58e7f536590b01bb737069e3d6d0521c9
dancer1325 <alfredotic0809@gmail.com>
2022-04-20 19:47:12 +0200

    Section 3

---
+++ Aeropuertos/4 - GraphQL Tools/README.md
@@ -0,0 +1,32 @@
+# How has the project been created?
+* `npm init -y`
+* `npm install env-cmd`
+    * Passing environment variables via '.env' file
+* `npm install --save-dev nodemon`
+* `npm install apollo-server`
+* `npm install graphql`
+* `npm install graphql-type-datetime`
+* `npm install graphql-tools`
+
+
+# How to run?
+* `npm install`
+* `npm run dev`
+  * Problems:
+    * Problem1: ' "Query" defined in resolvers, but not in schema'
+      * Solution: It's necessary to add 'type Query' in the '.gql' schema, but that will be fixed in next module
+
+# Notes
+* 'graphql'
+    * JS implementation for GraphQL
+* 'https://studio.apollographql.com/sandbox/explorer'
+    * Navigate to this url in your browser
+    * Your apollo server is hosted in that url
+    * You can execute the queries against GraphQL schema defined
+* 'typeDefs.gql'
+    * Define the graphql schema, but in this module, we don't use it yet
+* 'graphql-type-datetime'
+    * DateTime scalar type for GraphQL.js, following the ISO 8601 format.
+* 'graphql-tools'
+  * Set of tools to help to work with graphql
+
--- Aeropuertos/4 - GraphQL Tools/src/index.js
+++ Aeropuertos/4 - GraphQL Tools/src/index.js
@@ -1,23 +1,26 @@
 const { join } = require('path');
 const { ApolloServer } = require('apollo-server');
-const { loadTypedefsSync } = require('@graphql-tools/load');
-const { GraphQLFileLoader } = require('@graphql-tools/graphql-file-loader');
+const { loadTypedefsSync } = require('@graphql-tools/load');                    // graphql-tools' module
+const { GraphQLFileLoader } = require('@graphql-tools/graphql-file-loader');    // graphql-tools' module
 const GraphQLDateTime = require('graphql-type-datetime');
 
+// Load from local file
 const sources = loadTypedefsSync(join(__dirname, './typeDefs.gql'), {
     loaders: [
-        new GraphQLFileLoader()
+        new GraphQLFileLoader()         // One of the possible loaders https://www.graphql-tools.com/docs/api/interfaces/utils_src.Loader
     ]
 });
 
+// Get the source with all the documents indicated in the .gql
 const typeDefs = sources.map(source => source.document)
 
+// A map of functions to populate data for individual schema fields
 const resolvers = {
   Query: {},
 };
 
 const server = new ApolloServer({
-  typeDefs,
+  typeDefs,                 // Document or documents which represent your server's GraphQL schema
   resolvers,
 });
 
