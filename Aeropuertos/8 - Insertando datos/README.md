05b8ce5cf1f3359baf6b51c2376e69723e55ccd2
dancer1325 <alfredotic0809@gmail.com>
2022-04-22 01:20:51 +0200

    Section5, Chapter 1 and 2. Add commentaries.

---
+++ Aeropuertos/8 - Insertando datos/README.md
@@ -0,0 +1,52 @@
+# How has the project been created?
+* `npm init -y`
+* `npm install env-cmd`
+    * Passing environment variables via '.env' file
+* `npm install --save-dev nodemon`
+* `npm install apollo-server`
+* `npm install graphql`
+* `npm install graphql-type-datetime`
+* `npm install graphql-tools`
+* `npm install install`
+    * Handle the 'require' and 'exports'
+
+# How to run?
+* `npm install`
+* `npm run dev`
+* `npm run dev:noenv`
+    * Start up, without needing an '.env' file
+
+# How to execute GraphQL operations?
+* Steps
+    * Run your app
+    * Login in 'https://studio.apollographql.com/graph', create a graph, configure with the url endpoint
+    * Launch it the operation in the previous url
+
+# Notes
+* 'graphql'
+    * JS implementation for GraphQL
+* 'https://studio.apollographql.com/sandbox/explorer'
+    * Navigate to this url in your browser
+    * Your apollo server is hosted in that url
+    * You can execute the queries against GraphQL schema defined
+* 'typeDefs.gql'
+    * Define the graphql schema, but in this module, we don't use it yet
+* 'graphql-type-datetime'
+    * DateTime scalar type for GraphQL.js, following the ISO 8601 format.
+* 'graphql-tools'
+    * Set of tools to help to work with graphql
+* 'Queries' and 'Mutations' folder
+    * Folder to store our queries / mutations
+        * 1 query or mutation / file
+    * It will allow us triggering queries / mutations, based on the plugin downloaded in our IDE
+    * 'listarAeropuertos.gql', 'obtenerAeropuerto.gql', 'obtenerAeropuertoPorId.gql' and 'crearPasajero.gql'
+        * File for executing queries on VSC's plugin
+        * :warning: Not supported any more to execute queries using VSC's plugin
+    * TODO: Configure it for JetBrain's graphql plugin
+        * It yet supports the execution of queries and mutations operations
+* 'src/dataset.json'
+    * File generated in previous module, generated with 'faker' dependency
+* 'uid'
+  * Generate randomized output strings of fixed length using lowercase alphanumeric characters
+  * Used in our case, to create new ID for new objects
+  
--- Aeropuertos/8 - Insertando datos/src/index.js
+++ Aeropuertos/8 - Insertando datos/src/index.js
@@ -7,8 +7,9 @@ const GraphQLEnumType = require('graphql/type')
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
+// Read from the file created, instead of code the response directly here
+// JSON.parse(....)       JSON string file --> Object
 const aeropuertos = JSON.parse(fs.readFileSync(join(__dirname, './dataset.json'), 'utf8'));
-
 
 const sources = loadTypedefsSync(join(__dirname, './typeDefs.gql'), {
     loaders: [
@@ -19,24 +20,46 @@ const sources = loadTypedefsSync(join(__dirname, './typeDefs.gql'), {
 const typeDefs = sources.map(source => source.document)
 
 const resolvers = {
+  //Resolver for Query type in GraphQL
   Query: {
+    // NameOfTheFunction: LogicOfTheFunctionReturningSpecificType
     listarAeropuertos: () => {return aeropuertos},
+    // A resolver can have several arguments
+    // NameOfTheFunction(parent, args, context, info): LogicOfTheFunctionReturningSpecificType
     obtenerAeropuertoPorId: (obj, args) => {
-      const response = aeropuertos.filter(aeropuerto => {
-        aeropuerto.id === args.id;
+      console.log("args.id === aeropuertos[2].id ", args.id === aeropuertos[2].id);
+      // 1) Arrow functions
+      // TODO: Why filter isn't working?
+      // const response = aeropuertos.filter(aeropuerto => {
+      //   aeropuerto.id === args.id;
+      // });
+      // 2) Common function
+      const response = aeropuertos.filter(function (aeropuerto) {
+        return aeropuerto.id === args.id;
       });
+      console.log(response);
+
+      // As any typical handling errors, you can define all kind of business logic as you want
+      if (response.length === 0) {
+        throw 'Airport not found'
+      }
+
       return response[0];
     },
+    // You can send the arguments destructured also
     obtenerAeropuerto: (obj, {id, localizacion}) => {
+      // TODO: Why is arrow function working in this case?
       const response = aeropuertos.filter(aeropuerto => {
-        if (aeropuerto.localizacion === localizacion || aeropuerto.id === id){
+        if (aeropuerto.localizacion === localizacion || aeropuerto.id === id) {
           return aeropuerto;
         }
-      }); 
+      });
       return response[0];
     }
   },
+  //Resolver for Mutation type in GraphQL
   Mutation: {
+    // NameOfTheFunction(parent, args, context, info): LogicOfTheFunctionReturningSpecificType
     crearPasajero: (obj, {idAvion, nombre, apellido}) => {
       const pasajero = {id: uuidv4(), nombre, apellido}
       let insertado = false
